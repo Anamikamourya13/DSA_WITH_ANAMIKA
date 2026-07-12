fc46a757ccd44718108b363d0e3d3eb81b9c2cfe
Anamikamourya13 <anamikamourya686@gmail.com>
2026-07-13 00:05:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (6.75%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        vector<int> freq(26, 0);
+        unordered_map<char, int> have;
 
-        for(char ch : text) {
-            freq[ch - 'a']++;
+        for (char ch : text) {
+            have[ch]++;
         }
 
-        return min({
-            freq['b' - 'a'],
-            freq['a' - 'a'],
-            freq['l' - 'a'] / 2,
-            freq['o' - 'a'] / 2,
-            freq['n' - 'a']
-        });
+       
+        unordered_map<char, int> need;
+
+        need['b'] = 1;
+        need['a'] = 1;
+        need['l'] = 2;
+        need['o'] = 2;
+        need['n'] = 1;
+
+        int res = INT_MAX;
+
+        
+        for (auto p : need) {
+
+            char c = p.first;      // Character
+            int fneed = p.second;  // Required frequency
+            int fhave = have[c];   // Available frequency
+
+            int times = fhave / fneed;
+
+            res = min(res, times);
+        }
+
+        return res;
     }
 };
