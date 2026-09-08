df18341a133b60e4ce489634a024d6ca6b6cd755
Anamikamourya13 <anamikamourya686@gmail.com>
2026-09-08 23:14:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    unordered_map<int,int>dp;   //hashmap
+
     int fib(int n) {
         if (n==0)
         return 0;
 
          if(n==1)
          return 1;
-        
-        return fib(n - 1) + fib(n - 2);
+         
+         if(dp.find(n) != dp.end()) //already calculated
+           return dp[n];
+
+         dp[n] = fib(n - 1) + fib(n - 2); //shortly answer
+          return dp[n];
     }
 };
